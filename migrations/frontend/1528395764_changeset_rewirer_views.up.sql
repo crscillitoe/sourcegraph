979e1c95230fc4eea87ec373d7ad089a869bb6be
Erik Seliger <erikseliger@me.com>
2020-12-10 01:58:38 +0100

    Fix migration

--- migrations/frontend/1528395764_changeset_rewirer_views.up.sql
+++ migrations/frontend/1528395764_changeset_rewirer_views.up.sql
@@ -1,17 +1,27 @@
 BEGIN;
 
-CREATE VIEW changeset_spec_tracked_mappings AS (
-    SELECT changeset_specs.id AS changeset_spec_id, COALESCE(changesets.id, 0) AS changeset_id, changeset_specs.repo_id AS repo_id, changeset_specs.campaign_spec_id AS campaign_spec_id
+CREATE VIEW IF NOT EXISTS changeset_spec_tracked_mappings AS (
+    SELECT
+        changeset_specs.id AS changeset_spec_id,
+        COALESCE(changesets.id, 0) AS changeset_id,
+        changeset_specs.repo_id AS repo_id,
+        changeset_specs.campaign_spec_id AS campaign_spec_id
     FROM changeset_specs
     LEFT JOIN changesets ON changesets.repo_id = changeset_specs.repo_id AND changesets.external_id = changeset_specs.spec->>'externalID'
     INNER JOIN repo ON changeset_specs.repo_id = repo.id
     WHERE
-        changeset_specs.spec->>'externalID' IS NOT NULL AND changeset_specs.spec->>'externalID' != '' AND
+        changeset_specs.spec->>'externalID' IS NOT NULL AND
+        changeset_specs.spec->>'externalID' != '' AND
         repo.deleted_at IS NULL
 );
 
-CREATE VIEW changeset_spec_branch_mappings AS (
-    SELECT changeset_specs.id AS changeset_spec_id, COALESCE(changesets.id, 0) AS changeset_id, changeset_specs.repo_id AS repo_id, changeset_specs.campaign_spec_id AS campaign_spec_id, changesets.owned_by_campaign_id as owner_campaign_id
+CREATE VIEW IF NOT EXISTS changeset_spec_branch_mappings AS (
+    SELECT
+        changeset_specs.id AS changeset_spec_id,
+        COALESCE(changesets.id, 0) AS changeset_id,
+        changeset_specs.repo_id AS repo_id,
+        changeset_specs.campaign_spec_id AS campaign_spec_id,
+        changesets.owned_by_campaign_id AS owner_campaign_id
     FROM changeset_specs
     LEFT JOIN changesets ON
         changesets.repo_id = changeset_specs.repo_id AND
